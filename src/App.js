9bedd763b1f7ad96b574274626af635c740bde2b
farzad-nazif <farzadnazif.nz@gmail.com>
2022-11-21 19:13:53 +0000

    Level 1 completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import Card from "./Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      <h1>Highe Scores Per Country</h1>
+      <Card />
+    </Main>
   );
 }
 
+const Main = styled.div`
+  margin: 10rem 15rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  border-color: lightblue;
+  border-style: solid;
+`;
 export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import allCountryScores from "./scores";
+import styled from "styled-components";
+
+const Card = () => {
+  let scores = [];
+  allCountryScores.forEach((country) => {
+    scores.push(country.scores);
+  });
+  scores.map((item) => item.sort(({ s: a }, { s: b }) => b - a));
+  return (
+    <div>
+      {allCountryScores.map((country) => {
+        return (
+          <CardHolder>
+            <h1>HIGH SCORES: {country.name}</h1>
+            {country.scores.map((score) => {
+              return (
+                <Country>
+                  <h2>{score.n}:</h2>
+                  <h3>{score.s}</h3>
+                </Country>
+              );
+            })}
+          </CardHolder>
+        );
+      })}
+    </div>
+  );
+};
+
+const Country = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 1.2rem;
+  border-style: solid;
+  border-radius: 1.2rem;
+  padding: 1.2rem;
+  gap: 15rem;
+  min-width: 20rem;
+  h2 {
+    font-size: 2rem;
+  }
+  h3 {
+    font-size: 2rem;
+  }
+`;
+
+const CardHolder = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: lightblue;
+  border-radius: 2rem;
+  padding: 2rem 6rem;
+  margin: 2rem;
+`;
+
+export default Card;
